890a3dddc6703be8983d420e70d22e1035f55e7a
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-03-13 21:28:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 //your JS code here. If required.
-var myModal = document.getElementById('myModal')
-      var myInput = document.getElementById('myInput')
-
-      myModal.addEventListener('shown.bs.modal', function () {
-        myInput.focus()
-      })
